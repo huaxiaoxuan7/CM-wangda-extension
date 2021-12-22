7f1b6c6c2b41a8d952da4845c34074a880bcc774
huaxiaoxuan <hxx.edge7@gmail.com>
2021-12-22 12:58:17 +0800

    去掉了无用的文件

--- extension/background.js
+++ extension/background.js
@@ -1,5 +1,9 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('hi')
+chrome.runtime.onInstalled.addListener((reason) => {
+  // if (reason === chrome.runtime.OnInstalledReason.INSTALL) {
+  chrome.tabs.create({
+    url: 'welcome.html'
+  })
+  // }
 })
 
 chrome.runtime.onMessage.addListener(
@@ -15,5 +19,11 @@ chrome.runtime.onMessageExternal.addListener(
     sendResponse({ farewell: 'goodbye' })
   }
 )
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log('Request comes from content script ' + sender.tab.id)
+  if (request.greeting === 'close_tab') {
+    chrome.tabs.remove(sender.tab.id)
+  }
+})
 
 // chrome.storage.local.set({ panelPositionX: x, panelPositionY: y }, () => { })
