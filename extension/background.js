932beb85f13338052be7110bcd5654585753ca34
huaxiaoxuan <hxx.edge7@gmail.com>
2022-02-16 10:32:40 +0800

    添加了更新说明

--- extension/background.js
+++ extension/background.js
@@ -1,19 +1,24 @@
-chrome.runtime.onInstalled.addListener((reason) => {
-  chrome.storage.sync.set({
-    subjectList: [],
-    settings: [
-      { name: 'courseEnhanced', value: true },
-      { name: 'autoMute', value: true },
-      { name: 'autoCloseTab', value: false },
-      { name: 'homePanel', value: true },
-      { name: 'subjectPanel', value: true },
-      { name: 'coursePanel', value: true }
-    ]
-  })
-  chrome.tabs.create({ url: 'welcome.html' })
+chrome.runtime.onInstalled.addListener(({ reason }) => {
+  if (reason === 'install') {
+    chrome.storage.sync.set({
+      subjectList: [],
+      settings: [
+        { name: 'courseEnhanced', value: true },
+        { name: 'autoMute', value: true },
+        { name: 'autoCloseTab', value: false },
+        { name: 'homePanel', value: true },
+        { name: 'subjectPanel', value: true },
+        { name: 'coursePanel', value: true }
+      ]
+    })
+    chrome.tabs.create({ url: 'welcome.html' })
+  }
+  if (reason === 'update') {
+    chrome.tabs.create({ url: 'update.html' })
+  }
 })
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   const { action } = JSON.parse(request.payload)
   // 插件生效时
   if (action === 'on_load') {
